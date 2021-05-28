f7319b2cfd5143571e4db9a80df14fb58268cd35
humneankush <30699023+humneankush@users.noreply.github.com>
2021-05-28 09:48:38 +0530

    added 2.2 completed

---
+++ part2/part2.2/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import Course from "./components/Course";
+
+const App = (props) => {
+  const course = {
+    name: "Half Stack application development",
+    id: 1,
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h1>Web Development Curriculum</h1>
+      <Course course={course} />
+    </div>
+  );
+};
+
+export default App;
