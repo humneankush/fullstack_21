0786711d42ea9bdaaac91877b0a5baf7b9c043b1
humneankush <30699023+humneankush@users.noreply.github.com>
2021-06-07 12:11:03 +0530

    puhelinluettelon backend step3

---
+++ part3/part3.3/index.js
@@ -0,0 +1,53 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-64-23-122",
+  },
+];
+
+app.get("/api/info", (req, res) => {
+  const total = persons.length;
+  const date = new Date();
+  res.write(`phone has info of ${total} people.`);
+  res.write("\n");
+  res.write(`${date}`);
+  res.end();
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.json(`there is no record found with that ${id}`);
+  }
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+const PORT = 3001;
+app.listen(PORT);
+console.log(`listinig on port ${PORT}`);
